ef424d48a03744b864aea8cf1c837c8743f2da27
Moski Doski <moskidoski@Moskis-MacBook-Pro.local>
2019-08-27 22:59:18 +0400

    removing used rails system

--- config/application.rb
+++ config/application.rb
@@ -1,19 +1,21 @@
 require_relative "boot"
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+# only require Rails parts that we actually use, this shaves off some memory
+# ActiveStorage, ActionCable and TestUnit are not currently used by the app
+# see <https://github.com/rails/rails/blob/v5.2.3/railties/lib/rails/all.rb>
+%w[
+  active_model/railtie
+  active_record/railtie
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  sprockets/railtie
+  action_mailbox/engine
+  action_text/engine
+].each do |railtie|
+  require railtie
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
